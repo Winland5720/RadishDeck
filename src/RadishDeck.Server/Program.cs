43745a22d5ed9c92b5ec4598ac39682b66f16111
Winland5720 <Winland57@gmail.com>
2026-09-19 16:30:25 +0400

    Add server status endpoint

--- src/RadishDeck.Server/Program.cs
+++ src/RadishDeck.Server/Program.cs
@@ -1,4 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapGet("/status", () => new
+{
+    name = "Radish Deck Server",
+    version = "0.1.0",
+    status = "running"
+});
+
 app.Run();
